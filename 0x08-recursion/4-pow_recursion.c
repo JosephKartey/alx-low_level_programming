bab601ca5097686d3454512f871cf5396400e0ec
JosephKartey <karteyjosep1@gmail.com>
2024-02-01 13:19:51 +0000

    new2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,16 +4,16 @@
  * _pow_recursion - computes the power of a number
  * @x: base number
  * @y: exponent
-OBOBOB *
+ *
  * Return: result of x raised to the power of y
  */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1);
-    else if (y == 0)
-        return (1);
-    else
-        return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
 
